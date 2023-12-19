6f08823c84b342f5e923f14968656300a57b828b
jpassica <jammalpassica@gmail.com>
2023-12-19 19:24:37 +0200

    complete first stage of handling DB queries and requests

--- CreateDB.sql
+++ CreateDB.sql
@@ -10,41 +10,9 @@ CREATE TABLE Client (
     Phone_Number CHAR(11) NOT NULL
 );
 
-CREATE TABLE Customer(
-    Customer_ID INT PRIMARY KEY REFERENCES Client(Client_ID),
-    Points INT DEFAULT 0
-);
-
-CREATE TABLE Admin(
-    Admin_ID INT PRIMARY KEY REFERENCES Client(Client_ID)
-);
-
 CREATE TABLE ServiceCategory(
     ServiceCategory_ID SERIAL PRIMARY KEY,
     Name VARCHAR(45) NOT NULL
-);
-
-CREATE TABLE Technician(
-    Tech_ID INT PRIMARY KEY REFERENCES Client(Client_ID),
-    ServiceCategory_ID INT REFERENCES ServiceCategory(ServiceCategory_ID) NOT NULL,
-    Rating INT CHECK (Rating > 0 AND Rating < 6)  
-);
-
-CREATE TABLE Service(
-    Service_ID SERIAL PRIMARY KEY,
-    Service_status CHAR (8) NOT NULL CHECK (Service_status = 'PENDING' OR Service_status= 'UPCOMING' OR Service_status = 'FINISHED') DEFAULT 'PENDING',
-    Service_type CHAR(10) NOT NULL DEFAULT 'REGULAR',
-    Service_timestamp TIMESTAMP,
-    Duration INT,
-    Customer_ID INT REFERENCES Customer(Customer_ID)
-);
-
-CREATE TABLE Feedback(
-    Feedback_ID SERIAL PRIMARY KEY,
-    Content TEXT NOT NULL,
-    Feedback_timestamp TIMESTAMP,
-    Reporter_ID INT REFERENCES Client(Client_ID),
-    Reviewer_ID INT REFERENCES Admin(Admin_ID)
 );
 
 CREATE TABLE Point_System(
@@ -52,19 +20,76 @@ CREATE TABLE Point_System(
     Req_points INT NOT NULL
 );
 
+CREATE TABLE Customer(
+    Customer_ID INT PRIMARY KEY REFERENCES Client(Client_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    Points INT DEFAULT 0
+);
+
+CREATE TABLE Admin(
+    Admin_ID INT PRIMARY KEY REFERENCES Client(Client_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+
+CREATE TABLE Technician(
+    Tech_ID INT PRIMARY KEY REFERENCES Client(Client_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    ServiceCategory_ID INT NOT NULL REFERENCES ServiceCategory(ServiceCategory_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    Rating INT CHECK (Rating > 0 AND Rating < 6)  
+);
+
+CREATE TABLE Service(
+    Service_ID SERIAL PRIMARY KEY,
+    Service_status CHAR (8) NOT NULL CHECK (Service_status = 'PENDING' OR Service_status= 'UPCOMING' OR Service_status = 'FINISHED') DEFAULT 'PENDING',
+    Service_type CHAR(10) NOT NULL DEFAULT 'REGULAR',
+    Service_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    Duration INT,
+    Customer_ID INT REFERENCES Customer(Customer_ID)
+        ON DELETE CASCADE 
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE Feedback(
+    Feedback_ID SERIAL PRIMARY KEY,
+    Content TEXT NOT NULL,
+    Feedback_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    Reporter_ID INT REFERENCES Client(Client_ID)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE,
+    Reviewer_ID INT REFERENCES Admin(Admin_ID)  
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
 CREATE TABLE Reward(
-    Reward_ID INT PRIMARY KEY REFERENCES Service(Service_ID),
-    Customer_ID INT REFERENCES Customer(Customer_ID) NOT NULL,
-    Percentage INT REFERENCES Point_System(Percentage) NOT NULL
+    Reward_ID SERIAL PRIMARY KEY,
+    Customer_ID INT NOT NULL REFERENCES Customer(Customer_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    Percentage INT NOT NULL REFERENCES Point_System(Percentage)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE Complaint(
     Complaint_ID SERIAL PRIMARY KEY,
     Content TEXT NOT NULL,
-    Complaint_timestamp TIMESTAMP,
-    Customer_ID INT REFERENCES Customer(Customer_ID) NOT NULL,
-    Reviewer_ID INT REFERENCES Admin(Admin_ID) NOT NULL,
-    Service_ID INT REFERENCES Service(Service_ID) NOT NULL
+    Complaint_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    Customer_ID INT NOT NULL REFERENCES Customer(Customer_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    Reviewer_ID INT NOT NULL REFERENCES Admin(Admin_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    Service_ID INT NOT NULL REFERENCES Service(Service_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE Offer(
@@ -73,28 +98,43 @@ CREATE TABLE Offer(
     Price INT NOT NULL,
     Expiry_date DATE,
     Tech_ID INT REFERENCES Technician(Tech_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+
 );
 
 CREATE TABLE Review(
     Review_ID SERIAL PRIMARY KEY,
     Rating INT CHECK (Rating > 0 AND Rating < 6),
-    Service_ID INT REFERENCES Service(Service_ID) NOT NULL,
-    Customer_ID INT REFERENCES Customer(Customer_ID) NOT NULL,
+    Service_ID INT NOT NULL REFERENCES Service(Service_ID) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    Customer_ID INT NOT NULL REFERENCES Customer(Customer_ID)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE,
     Content TEXT
 );
 
 CREATE TABLE Bundle(
     Bundle_ID SERIAL PRIMARY KEY,
-    Creator_ID INT REFERENCES Admin(Admin_ID) NOT NULL,
+    Creator_ID INT NOT NULL REFERENCES Admin(Admin_ID)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE,
     Expiry_date DATE,
     Total_price INT NOT NULL, 
     Services_count INT
 );
 
 CREATE TABLE ConsistOf(
-    Service_ID INT REFERENCES Service(Service_ID),
-    ServiceCategory_ID INT REFERENCES ServiceCategory(ServiceCategory_ID),
-    Tech_ID INT REFERENCES Technician(Tech_ID),
+    Service_ID INT REFERENCES Service(Service_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    ServiceCategory_ID INT REFERENCES ServiceCategory(ServiceCategory_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    Tech_ID INT REFERENCES Technician(Tech_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
     PRIMARY KEY (Service_ID, ServiceCategory_ID, Tech_ID)
 );
 
@@ -102,23 +142,37 @@ CREATE TABLE Notification(
     Notification_ID SERIAL PRIMARY KEY,
     Content TEXT NOT NULL,
     Notification_timestamp TIMESTAMP,
-    Notified_ID INT REFERENCES Client(Client_ID) NOT NULL,
-    Service_ID INT REFERENCES Service(Service_ID) NOT NULL
+    Notified_ID INT NOT NULL REFERENCES Client(Client_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    Service_ID INT NOT NULL REFERENCES Service(Service_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE IsBundle(
-    Service_ID INT REFERENCES Service(Service_ID),
-    Bundle_ID INT REFERENCES Bundle(Bundle_ID),
+    Service_ID INT REFERENCES Service(Service_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    Bundle_ID INT REFERENCES Bundle(Bundle_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
     PRIMARY KEY (Service_ID, Bundle_ID)
 );
 
 CREATE TABLE RegularService(
-    Service_ID INT REFERENCES Service(Service_ID),
+    Service_ID INT REFERENCES Service(Service_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
     Price INT NOT NULL
 );
 
 CREATE TABLE IsOffer(
-    Service_ID INT REFERENCES Service(Service_ID),
-    Offer_ID INT REFERENCES Offer(Offer_ID),
+    Service_ID INT REFERENCES Service(Service_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    Offer_ID INT REFERENCES Offer(Offer_ID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
     PRIMARY KEY (Service_ID, Offer_ID)
 );
--- Server/server.js
+++ Server/server.js
@@ -13,18 +13,206 @@ const db = new pg.Client({
     port: 5432
 });
 
-app.get("/SignUp", (req, res) => {
-    if (req.body["type" == "Customer"])
-        res.redirect("/SignUp/Customer");
-});
-
-app.get("/SignUp", (req, res) => {
-    db.query("INSERT INTO Customer (FullName, ) VALUES", []);
-});
-
 db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Sign In Customer or Tech or Admin
+app.post("/SignIn", async(req, res) => {
+    try {
+        const result = await db.query(`SELECT fullname,type FROM client WHERE LOWER(email) = '${req.body["email"].toLowerCase()}' AND password = '${req.body["password"]}'`);
+        console.log(result.rows[0].fullname);
+        res.send(`Successfully logged in, ${result.rows[0].fullname}!`);
+
+        //res.render
+
+        if (result.rows[0].type == 'c')
+            console.log("Customer logged in.");
+        else if (result.rows[0].type == 't')
+            console.log("Tech logged in.");
+        else 
+            console.log("Admin logged in.");
+        
+    } catch(error) {
+        res.send("Wrong email or password!");
+        console.log(error);
+    }
+});
+
+// Sign Up Customer and Tech
+app.post("/SignUp", async (req, res) => {
+    // Validate unique email
+
+    if ((await db.query(`SELECT * FROM client WHERE email = '${req.body.email}';`)).rowCount != 0)
+    {
+        console.log("Sign up denied");
+        return res.send("This email is taken!");
+    }
+
+    try{
+       await db.query("INSERT INTO client (email, address, password, gender, phone_number, fullname, type)"+
+       " VALUES ($1, $2, $3, $4, $5, $6, $7)", 
+        [req.body["email"], req.body["address"], req.body["password"], 
+        req.body["gender"], req.body["phone_number"], req.body["fullname"], req.body["type"]]);
+
+        //res.send("client inserted successfully!");
+    } catch (error) {
+        res.send("Couldn't insert client!");
+        console.log(error);
+    }
+
+    if (req.body["type"] === 'c')
+    {
+        const new_id = (await db.query("SELECT currval('client_client_id_seq');")).rows[0].currval;;
+
+        try{
+            await db.query(`INSERT INTO customer VALUES (${new_id}, 0);`);
+            res.send("customer inserted successfully!");
+        }
+        catch (error){
+            res.send("Couldn't insert customer!");
+            console.log(error);
+        }
+    } else if (req.body["type"] === 't')
+    {
+        const new_id = (await db.query("SELECT currval('client_client_id_seq');")).rows[0].currval;;
+
+
+        const service_query = await db.query(`SELECT servicecategory_id FROM servicecategory WHERE name = '${req.body["servicecategory"]}'`);
+        const servicecategory_id = service_query.rows[0].servicecategory_id;
+
+        try{
+            await db.query(`INSERT INTO technician (tech_id, servicecategory_id) VALUES (${new_id}, ${servicecategory_id});`);
+            res.send("technician inserted successfully!");
+        }
+        catch (error){
+            res.send("Couldn't insert technician!");
+            console.log(error);
+        }
+    } 
+    
+});
+
 app.listen(port, () => {
     console.log(`Server is listening at port ${port}`);
+});
+
+// Show techs in area by service category
+app.get("/Order/AvailableTechs", async (req, res) => {
+    const customerCity = req.body.city; //how to figure this out?
+    const s_category = req.body.s_category;
+    const result = await db.query(`SELECT fullname, name, rating FROM client, technician, servicecategory
+        WHERE address = '${customerCity}' AND technician.servicecategory_id = servicecategory.servicecategory_id
+        AND tech_id = client_id AND name = '${s_category}'`);
+    res.send(result.rows);
+    console.log(req.body);
+});
+
+// Show Past Orders 
+app.get("/Orders/Past", async (req, res) => {
+    const c_id = req.body["c_id"]; 
+    const result = await db.query(`SELECT service_timestamp, name FROM service, consistof, servicecategory WHERE customer_id = ${c_id}
+        AND consistof.service_id = service.service_id AND servicecategory.servicecategory_id = consistof.servicecategory_id
+        AND service_status = 'FINISHED';`);
+    res.send(result.rows);
+}); 
+
+// Show Pending Orders 
+app.get("/Orders/Pending", async (req, res) => {
+    const c_id = req.body["c_id"]; 
+    const result = await db.query(`SELECT service_timestamp, name FROM service, consistof, servicecategory WHERE customer_id = ${c_id}
+        AND consistof.service_id = service.service_id AND servicecategory.servicecategory_id = consistof.servicecategory_id
+        AND service_status = 'PENDING';`);
+    res.send(result.rows);
+}); 
+
+// Make Regular Order
+app.post("/Order", async (req, res) => {
+
+    const c_id = req.body.c_id;
+    const t_id = req.body.t_id;
+    try {
+        await db.query("INSERT INTO service (duration, customer_id)"+ 
+        "VALUES ($1, $2)", [3, c_id]);
+
+        const s_id = (await db.query("SELECT currval('service_service_id_seq');")).rows[0].currval;
+
+        const category = (await db.query("SELECT servicecategory_id FROM technician WHERE tech_id = $1", [t_id])).rows[0].servicecategory_id;
+
+        await db.query("INSERT INTO consistof (service_id, servicecategory_id, tech_id) VALUES ($1, $2, $3)", [s_id, category, t_id]);
+
+        res.send("service order created succesfully!");
+    }
+    catch (error) {
+        res.send("couldn't create service order!");
+        console.log(error);
+    }
+   
+});
+
+// Review an order
+app.post("/Order/Review", async (req, res) => {
+    const s_id = req.body.s_id;
+    const c_id = (await db.query("SELECT customer_id FROM service WHERE service_id = $1;", [s_id])).rows[0].customer_id;
+
+    // Customer can only make one review about each service 
+    if ((await db.query("SELECT * FROM review WHERE service_id = $1 AND customer_id = $2;", [s_id, c_id])).rowCount != 0 )
+    {
+        return res.send("You have already reviewed this service, you cannot review it again.");
+    }
+
+    try {
+        await db.query("INSERT INTO review (rating, service_id, customer_id, content) VALUES ($1, $2, $3, $4);", 
+        [req.body.rating, s_id, c_id, req.body.content]);
+        res.send("Review posted successfully!");
+    } catch (error) {
+        res.send("Could not post review!");
+        console.log(error);
+    }
+        
+});
+
+// Make a complaint about order
+app.post("/Order/Complain", async (req, res) => {
+    // Should only need s_id to identify c_id
+    const s_id = req.body.s_id;
+    const c_id = (await db.query("SELECT customer_id FROM service WHERE service_id = $1;", [s_id])).rows[0].customer_id;
+    const a_id = req.body.a_id; // How to determine the admin that would oversee??
+
+    try {
+        await db.query("INSERT INTO complaint (content, customer_id, reviewer_id, service_id) VALUES ($1, $2, $3, $4);", 
+        [req.body.content, c_id, a_id, s_id]);
+
+    res.send("Complaint is sent successfully, help is on the way!");
+    } catch (error) {
+        console.log(error);
+        res.send("Could not make complaint!");
+    }
+});
+
+// Win a Reward
+app.post("/CustomerProfile/Reward", async (req, res) => {
+    const c_id = req.body.c_id;
+    const prcntg = req.body.percentage; // this of course is not determined this way
+
+    try{
+        await db.query("INSERT INTO reward (customer_id, percentage) VALUES ($1, $2);", [c_id, prcntg]);
+        res.send("Congrats! You have won this reward!");
+    } catch (error) {
+        res.send("Could not win reward!");
+        console.log(error);
+    }
+});
+
+// Give feedback
+app.post("/feedback", async (req, res) => {
+    const a_id = req.body.a_id;
+    const r_id = req.body.r_id;
+    
+    try {
+        await db.query("INSERT INTO feedback (content, reporter_id, reviewer_id) VALUES ($1, $2, $3);", [req.body.content, r_id, a_id]);
+        res.send("Your feedback has been received!");
+    } catch (error) {
+        res.send("Could not record your feedback!");
+        console.log(error);
+    }
 });
